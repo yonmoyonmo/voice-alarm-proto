dc4af707717053371e662591135d75c8275c006e
yonmoyonmo <yonmosound@gmail.com>
2021-10-20 05:02:48 +0900

    Initial Commit

---
+++ voice-alarm-shit/voice_alarm_shitApp.swift
@@ -0,0 +1,17 @@
+//
+//  voice_alarm_shitApp.swift
+//  voice-alarm-shit
+//
+//  Created by yonmo on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct voice_alarm_shitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
